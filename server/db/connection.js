5ce58a35bba23588ca09b0425d4e71d431def764
Shane Tajima <shanetajima@gmail.com>
2018-09-10 11:14:10 -0700

    Setup eslint with precommit hook and mod files to comply

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,19 +1,18 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 const mlab = 'mongodb://zagat:zagatnavbar1!@ds149742.mlab.com:49742/fec-zagat-reviews';
+const options = { useNewUrlParser: true };
 
-const connection = MongoClient.connect(
-  mlab,
-  { useNewUrlParser: true },
-  (error, db) => {
-    if (error) {
-      console.log('Error connecting to the DB', error);
-    } else {
-      console.log('Connected successfully to mlab');
-      return db.db('fec-zagat-reviews');
-    }
-  },
-);
+const handleConnection = (error, db) => {
+  if (error) {
+    // console.log('Error connecting to the DB', error);
+    return error;
+  }
+  // console.log('Connected successfully to mlab');
+  return db.db('fec-zagat-reviews');
+};
+
+const connection = MongoClient.connect(mlab, options, handleConnection);
 
 module.exports = {
   db: connection,
--- server/server.js
+++ server/server.js
@@ -1,60 +1,57 @@
 const express = require('express');
-const { db } = require('./db/connection');
+const { MongoClient } = require('mongodb');
+
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
 const mlab = 'mongodb://zagat:zagatnavbar1!@ds149742.mlab.com:49742/fec-zagat-reviews';
 
-///////////////////////////////////////////
 // Config
 const PORT = process.env.PORT || 8000;
-///////////////////////////////////////////
+const mongoOptions = { useNewUrlParser: true };
 // Models
 const selectRestaurant = (id, callback) => {
-  MongoClient.connect(
-    mlab,
-    { useNewUrlParser: true },
-    (error, db) => {
-      if (error) {
-        console.log('Error connecting to the DB', error);
-      } else {
-        console.log('Connected successfully to mlab');
-        const dbo = db.db('fec-zagat-reviews');
-        dbo.collection('restaurants').findOne({ id: Number(id) }, (error, result) => {
-          if (error) {
-            callback(error);
-          } else {
-            console.log('Got restaurant', result);
-            callback(null, result);
-          }
-          db.close();
-        });
-      }
-    },
-  );
+  const select = (connError, db) => {
+    if (connError) {
+      // console.log('Error connecting to the DB', error);
+      callback(connError);
+    } else {
+      // console.log('Connected successfully to mlab');
+      const dbo = db.db('fec-zagat-reviews');
+      dbo.collection('restaurants').findOne({ id: Number(id) }, (findError, result) => {
+        if (findError) {
+          callback(findError);
+        } else {
+          // console.log('Got restaurant', result);
+          callback(null, result);
+        }
+        db.close();
+      });
+    }
+  };
+  MongoClient.connect(mlab, mongoOptions, select);
 };
-///////////////////////////////////////////
+
 // Controllers
 const getRestaurant = (req, res) => {
-  console.log('Getting restaurant');
-  const id = req.params.id;
+  // console.log('Getting restaurant');
+  const { id } = req.params;
   selectRestaurant(id, (error, restaurant) => {
     if (error) {
       res.status(400).end();
     } else {
-      console.log('Get successful');
+      // console.log('Get successful');
       res.status(200).json(restaurant);
     }
   });
 };
-///////////////////////////////////////////
+
 // Routes
 app.get('/api/restaurant/:id', getRestaurant);
-///////////////////////////////////////////
+
 // Start
 app.listen(PORT, (error) => {
   if (error) {
-    console.log('Failed to start server');
+    // console.log('Failed to start server');
   } else {
-    console.log('Server started on', PORT);
+    // console.log('Server started on', PORT);
   }
 });
