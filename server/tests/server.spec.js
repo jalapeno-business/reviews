7091b4c2fcb2428351a5645f598e7ac57159e492
Shane Tajima <shanetajima@gmail.com>
2018-09-11 09:28:21 -0700

    A working test!!

--- server/tests/server.spec.js
+++ server/tests/server.spec.js
@@ -1,12 +1,39 @@
 const request = require('supertest');
+const mongoWrapper = require('../mongoose/db.js');
 const app = require('../app.js');
 
-describe('GET /api/restaurant', () => {
-  it('should get a restaurant', (done) => {
+// describe('GET /api/restaurant', () => {
+//   it('should get a restaurant', (done) => {
+//     request(app)
+//       .get('/api/restaurant/1')
+//       .then((response) => {
+//         console.log(response.statusCode);
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//     // .then(mongoose.disconnect)
+//     // .then(done);
+//     // request(app)
+//     //   .get('/api/restaurant/1')
+//     //   .set('Accept', 'application/json')
+//     //   .expect('Content-Type', /json/)
+//     //   .expect(200, done);
+//   });
+// });
+
+
+describe('Get a restaurant', () => {
+  beforeAll(() => {
+    mongoWrapper.connect();
+  });
+
+  test('It should return 200', () => {
     request(app)
       .get('/api/restaurant/1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
+      .then(res => expect(res.statusCode).toBe(200));
+  });
+
+  afterAll((done) => {
+    mongoWrapper.disconnect(done);
   });
 });
