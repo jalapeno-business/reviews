66f36039d5d99be76b04ebcce84535de02ff81a6
Shane Tajima <shanetajima@gmail.com>
2018-09-11 20:44:06 -0700

    Jest working with forceExit

--- server/tests/server.spec.js
+++ server/tests/server.spec.js
@@ -2,38 +2,39 @@ const request = require('supertest');
 const mongoWrapper = require('../mongoose/db.js');
 const app = require('../app.js');
 
-// describe('GET /api/restaurant', () => {
-//   it('should get a restaurant', (done) => {
-//     request(app)
-//       .get('/api/restaurant/1')
-//       .then((response) => {
-//         console.log(response.statusCode);
-//         expect(response.statusCode).toBe(200);
-//         done();
-//       });
-//     // .then(mongoose.disconnect)
-//     // .then(done);
-//     // request(app)
-//     //   .get('/api/restaurant/1')
-//     //   .set('Accept', 'application/json')
-//     //   .expect('Content-Type', /json/)
-//     //   .expect(200, done);
-//   });
-// });
-
-
 describe('Get a restaurant', () => {
   beforeAll(() => {
     mongoWrapper.connect();
   });
 
-  test('It should return 200', () => {
-    request(app)
-      .get('/api/restaurant/1')
-      .then(res => expect(res.statusCode).toBe(200));
-  });
-
   afterAll((done) => {
     mongoWrapper.disconnect(done);
+  });
+
+  test('It should have an id', () => {
+    request(app)
+      .get('/api/restaurant/1')
+      .then(res => JSON.parse(res.text))
+      .then((restaurant) => {
+        expect(restaurant).toHaveProperty('_id');
+      });
+  });
+
+  test('It should have reviews', () => {
+    request(app)
+      .get('/api/restaurant/1')
+      .then(res => JSON.parse(res.text))
+      .then((restaurant) => {
+        expect(restaurant).toHaveProperty('reviews');
+      });
+  });
+
+  test('It should have 0 to many reviews', () => {
+    request(app)
+      .get('/api/restaurant/1')
+      .then(res => JSON.parse(res.text))
+      .then((restaurant) => {
+        expect(Array.isArray(restaurant.reviews)).toBe(true);
+      });
   });
 });
